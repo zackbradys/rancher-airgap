379e8f69f4fecb55c82e90c164e5e39b75780657
Zack Brady <zackbrady123@gmail.com>
2022-12-15 19:42:10 -0500

    updated formatting

--- rancher-stack/rancher-offline-installer.sh
+++ rancher-stack/rancher-offline-installer.sh
@@ -25,7 +25,7 @@ export NC='\x1b[0m'
 # Base Settings
 function base-settings () {
 
-echo -e "${BLUE}Rancher Installer:${NC}- Updating Kernel Settings"
+echo -e "${BLUE}Rancher Offline Installer:${NC}- Updating Kernel Settings"
 cat << EOF >> /etc/sysctl.conf
 
 # SWAP Settings
@@ -78,7 +78,7 @@ fs.inotify.max_user_watches=1048576
 EOF
 sysctl -p > /dev/null 2>&1
 
-  echo -e "${BLUE}Rancher Installer:${NC}- Installing Base Packages"
+  echo -e "${BLUE}Rancher Offline Installer:${NC}- Installing Base Packages"
   yum install -y zstd nfs-utils iptables skopeo container-selinux iptables libnetfilter_conntrack libnfnetlink libnftnl policycoreutils-python-utils cryptsetup iscsi-initiator-utils
   systemctl enable iscsid && systemctl start iscsid
   echo -e "[keyfile]\nunmanaged-devices=interface-name:cali*;interface-name:flannel*" > /etc/NetworkManager/conf.d/rke2-canal.conf
@@ -87,13 +87,13 @@ sysctl -p > /dev/null 2>&1
 # Build/Staging Setup
 function build-server () {
 
-  echo -e "${BLUE}Rancher Installer ${NC}- Installing Skopeo and ZSTD"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- Installing Skopeo and ZSTD"
   yum install zstd skopeo -y > /dev/null 2>&1
 
   mkdir -p /opt/rancher/rke2_$RKE_VERSION/
   cd /opt/rancher/rke2_$RKE_VERSION/
 
-  echo -e "${BLUE}Rancher Installer ${NC}- Fetching RKE2"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- Fetching RKE2"
   curl -#OL https://github.com/rancher/rke2/releases/download/v$RKE_VERSION%2Brke2r1/rke2-images.linux-amd64.tar.zst
   curl -#OL https://github.com/rancher/rke2/releases/download/v$RKE_VERSION%2Brke2r1/rke2.linux-amd64.tar.gz
   curl -#OL https://github.com/rancher/rke2/releases/download/v$RKE_VERSION%2Brke2r1/sha256sum-amd64.txt
@@ -101,37 +101,37 @@ function build-server () {
   curl -#OL https://github.com/rancher/rke2-selinux/releases/download/v0.9.stable.1/rke2-selinux-0.9-1.el8.noarch.rpm
   curl -sfL https://get.rke2.io -o install.sh
 
-  echo -e "${BLUE}Rancher Installer ${NC}- Setup Helm"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- Setup Helm"
   mkdir -p /opt/rancher/helm/
   cd /opt/rancher/helm/
 
-  echo -e "${BLUE}Rancher Installer ${NC}- Installing Helm"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- Installing Helm"
   curl -#LO https://get.helm.sh/helm-v3.10.2-linux-386.tar.gz > /dev/null 2>&1
   tar -zxvf helm-v3.10.2-linux-386.tar.gz > /dev/null 2>&1
   rsync -avP linux-386/helm /usr/local/bin/ > /dev/null 2>&1
   rm -rf linux-386 > /dev/null 2>&1
 
-  echo -e "${BLUE}Rancher Installer ${NC}- Fetching Repositories"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- Fetching Repositories"
   helm repo add jetstack https://charts.jetstack.io > /dev/null 2>&1
   helm repo add rancher-latest https://releases.rancher.com/server-charts/latest > /dev/null 2>&1
   helm repo add longhorn https://charts.longhorn.io > /dev/null 2>&1
   helm repo add neuvector https://neuvector.github.io/neuvector-helm/ > /dev/null 2>&1
   helm repo update > /dev/null 2>&1
 
-  echo -e "${BLUE}Rancher Installer ${NC}- Fetching Helm Charts"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- Fetching Helm Charts"
   helm pull jetstack/cert-manager --version $CERT_VERSION > /dev/null 2>&1
   helm pull rancher-latest/rancher --version $RANCHER_VERSION > /dev/null 2>&1
   helm pull longhorn/longhorn --version $LONGHORN_VERSION > /dev/null 2>&1
   helm pull neuvector/core --version $NEUVECTOR_VERSION > /dev/null 2>&1
 
-  echo -e "${BLUE}Rancher Installer ${NC}- Setup Image Directory"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- Setup Image Directory"
   mkdir -p /opt/rancher/images/{cert,rancher,longhorn,registry,neuvector}
   cd /opt/rancher/images/
 
-  echo -e "${BLUE}Rancher Installer ${NC}- Fetching Rancher Images"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- Fetching Rancher Images"
   curl -#L https://github.com/rancher/rancher/releases/download/$RANCHER_VERSION/rancher-images.txt -o rancher/orig_rancher-images.txt
 
-  echo -e "${BLUE}Rancher Installer ${NC}- Clean Up Rancher Images"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- Clean Up Rancher Images"
   sed -i -e '0,/busybox/s/busybox/library\/busybox/' -e 's/registry/library\/registry/g' rancher/orig_rancher-images.txt
   sed -i -E '/neuvector|minio|gke|aks|eks|sriov|harvester|mirrored|longhorn|thanos|tekton|istio|multus|hyper|jenkins|windows/d' rancher/orig_rancher-images.txt
   for i in $(cat rancher/orig_rancher-images.txt|awk -F: '{print $1}'); do 
@@ -139,44 +139,44 @@ function build-server () {
   done
   cat rancher/version_unsorted.txt | sort -u >> rancher/rancher-images.txt
 
-  echo -e "${BLUE}Rancher Installer ${NC}- Clean Up Cert Manager Images"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- Clean Up Cert Manager Images"
   helm template /opt/rancher/helm/cert-manager-$CERT_VERSION.tgz | awk '$1 ~ /image:/ {print $2}' | sed s/\"//g > cert/cert-manager-images.txt
 
-  echo -e "${BLUE}Rancher Installer ${NC}- Clean Up Longhorn Images"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- Clean Up Longhorn Images"
   curl -#L https://raw.githubusercontent.com/longhorn/longhorn/$LONGHORN_VERSION/deploy/longhorn-images.txt -o longhorn/longhorn-images.txt
 
-  echo -e "${BLUE}Rancher Installer ${NC}- Clean Up Neuvector Images"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- Clean Up Neuvector Images"
   helm template /opt/rancher/helm/core-$NEUVECTOR_VERSION.tgz | awk '$1 ~ /image:/ {print $2}' | sed -e 's/\"//g' > neuvector/neuvector_images.txt
 
-  echo -e "${BLUE}Rancher Installer ${NC}- Fetching Images"
-  echo -e "${BLUE}Rancher Installer ${NC}- ... Registry Images"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- Fetching Images"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- ... Registry Images"
   for i in $(cat cert/cert-manager-images.txt); do 
     skopeo copy docker://$i docker-archive:cert/$(echo $i| awk -F/ '{print $3}'|sed 's/:/_/g').tar:$(echo $i| awk -F/ '{print $3}') > /dev/null 2>&1
   done
 
-  echo -e "${BLUE}Rancher Installer ${NC}- .... Neuvector Images"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- .... Neuvector Images"
   for i in $(cat neuvector/neuvector_images.txt); do 
     skopeo copy docker://$i docker-archive:neuvector/$(echo $i| awk -F/ '{print $3}'|sed 's/:/_/g').tar:$(echo $i| awk -F/ '{print $3}') > /dev/null 2>&1
   done
 
-  echo -e "${BLUE}Rancher Installer ${NC}- ..... Longhorn Images"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- ..... Longhorn Images"
   for i in $(cat longhorn/longhorn-images.txt); do 
     skopeo copy docker://$i docker-archive:longhorn/$(echo $i| awk -F/ '{print $2}'|sed 's/:/_/g').tar:$(echo $i| awk -F/ '{print $2}') > /dev/null 2>&1
   done
 
-  echo -e "${BLUE}Rancher Installer ${NC}- ...... Rancher Images"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- ...... Rancher Images"
   for i in $(cat rancher/rancher-images.txt); do 
     skopeo copy docker://$i docker-archive:rancher/$(echo $i| awk -F/ '{print $2}'|sed 's/:/_/g').tar:$(echo $i| awk -F/ '{print $2}') > /dev/null 2>&1
   done
 
-  echo -e "${BLUE}Rancher Installer ${NC}- ...... Fetching Registry"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- Fetching Registry"
   curl -#L https://raw.githubusercontent.com/zackbradys/rancher-offline-install/main/rancher-stack/registry.tar -o registry/registry_2.tar > /dev/null 2>&1
 
-  echo -e "${BLUE}Rancher Installer ${NC}- Compressing... this might take a hot minute"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- Compressing... this might take a hot minute"
   cd /opt/rancher/
   tar -I zstd -vcf /opt/rke2-rancher-longhorn-neuvector.zst $(ls) > /dev/null 2>&1
 
-  echo -e "${BLUE}Rancher Installer ${NC}- Mount/Copy/Move to Control Node Server"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- Mount/Copy/Move to Control Node Server"
   echo -e "  Mount/Copy/Move, will depend on your environment."
   echo -e "  Tip - To uncompress, run the following commands:"
   echo -e "    yum install -y zstd"
@@ -185,12 +185,12 @@ function build-server () {
 }
 
 function deploy-control () {
-  echo -e "${BLUE}Rancher Installer ${NC}- Deploying Control Node"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- Deploying Control Node"
 
   # Setup Base
   base-settings
 
-  echo -e "${BLUE}Rancher Installer ${NC}- Installing RKE2"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- Installing RKE2"
   cd /opt/rancher/rke2_$RKE_VERSION
   useradd -r -c "etcd user" -s /sbin/nologin -M etcd -U
   mkdir -p /etc/rancher/rke2/ /var/lib/rancher/rke2/server/manifests/
@@ -205,18 +205,18 @@ function deploy-control () {
   yum install -y /opt/rancher/rke2_$RKE_VERSION/rke2-common-$RKE_VERSION.rke2r1-0.x86_64.rpm /opt/rancher/rke2_$RKE_VERSION/rke2-selinux-0.9-1.el8.noarch.rpm
   systemctl enable rke2-server.service && systemctl start rke2-server.service
 
-  echo -e "${BLUE}Rancher Installer ${NC}- Waiting for RKE2..."
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- Waiting for RKE2..."
   sleep 60
 
   echo "export KUBECONFIG=/etc/rancher/rke2/rke2.yaml CRI_CONFIG_FILE=/var/lib/rancher/rke2/agent/etc/crictl.yaml PATH=$PATH:/var/lib/rancher/rke2/bin" >> ~/.bashrc
   ln -s /var/run/k3s/containerd/containerd.sock /var/run/containerd/containerd.sock
   source ~/.bashrc
 
-  echo -e "${BLUE}Rancher Installer ${NC}- Setup NFS/Mount for Worker Nodes"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- Setup NFS/Mount for Worker Nodes"
   echo "/opt/rancher *(ro)" > /etc/exports
   systemctl enable nfs-server.service && systemctl start nfs-server.service
 
-  echo -e "${BLUE}Rancher Installer ${NC}- Start Local Registry"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- Start Local Registry"
   mkdir /opt/rancher/registry
   chcon system_u:object_r:container_file_t:s0 /opt/rancher/registry
 
@@ -256,10 +256,10 @@ spec:
       hostNetwork: true
 EOF
 
-  echo -e "${BLUE}Rancher Installer ${NC}- Waiting for Kubernetes..."
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- Waiting for Kubernetes..."
   sleep 60
   
-  echo -e "${BLUE}Rancher Installer ${NC}- Load Images to Local Registry"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- Load Images to Local Registry"
   for file in $(ls /opt/rancher/images/longhorn/ | grep -v txt ); do 
     skopeo copy docker-archive:/opt/rancher/images/longhorn/$file docker://$(echo $file | sed 's/.tar//g' | awk -F_ '{print "localhost:5000/longhornio/"$1":"$2}') --dest-tls-verify=false
   done
@@ -281,14 +281,14 @@ EOF
 
   chmod 600 /etc/rancher/rke2/rke2.yaml
 
-  echo -e "${BLUE}Rancher Installer ${NC}- Unpacking Helm"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- Unpacking Helm"
   cd /opt/rancher/helm
   tar -zxvf helm-v3.10.2-linux-386.tar.gz > /dev/null 2>&1
   rsync -avP linux-386/helm /usr/local/bin/ > /dev/null 2>&1
 
   cat /var/lib/rancher/rke2/server/token > /opt/rancher/token
 
-  echo -e "${BLUE}Rancher Installer ${NC}- Mount Directory to Worker Nodes"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- Mount Directory to Worker Nodes"
   echo -e "  Switch to your worker nodes and run the following commands:"
   echo -e "    mkdir /opt/rancher"
   echo -e "    mount $(hostname -I | awk '{ print $1 }'):/opt/rancher /opt/rancher"
@@ -299,7 +299,7 @@ EOF
 }
 
 function deploy-worker () {
-  echo -e "${BLUE}Rancher Installer ${NC}- Deploying Worker Node"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- Deploying Worker Node"
 
   # Verify Control Node Mount Point
   if [ ! -f /opt/rancher/token ]; then echo " -$RED Did you mount the volume from the first node?$NO_COLOR"; exit 1; fi
@@ -328,37 +328,37 @@ function deploy-worker () {
 
 # Install and Deploy Rancher
 function install-rancher () {
-  echo -e "${BLUE}Rancher Installer ${NC}- Deploying Cert Manager"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- Deploying Cert Manager"
   helm upgrade -i cert-manager /opt/rancher/helm/cert-manager-v1.10.0.tgz --namespace cert-manager --create-namespace --set installCRDs=true --set image.repository=localhost:5000/cert-manager-controller --set webhook.image.repository=localhost:5000/cert-manager-webhook --set cainjector.image.repository=localhost:5000/cert-manager-cainjector --set startupapicheck.image.repository=localhost:5000/cert-manager-ctl
   
-  echo -e "${BLUE}Rancher Installer ${NC}- Deploying Rancher"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- Deploying Rancher"
   helm upgrade -i rancher /opt/rancher/helm/rancher-2.7.0.tgz --namespace cattle-system --create-namespace --set bootstrapPassword=bootStrapAllTheThings --set replicas=1 --set auditLog.level=2 --set auditLog.destination=hostPath --set useBundledSystemChart=true --set rancherImage=localhost:5000/rancher/rancher --set systemDefaultRegistry=localhost:5000 --set hostname=rancher.$DOMAIN
   
-  echo -e "${BLUE}Rancher Installer ${NC}- Bootstrap Password"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- Bootstrap Password"
   echo "  - \"bootStrapAllTheThings\" "
 }
 
 # Install and Deploy Longhorn
 function install-longhorn () {
-  echo -e "${BLUE}Rancher Installer ${NC}- Deploying Longhorn"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- Deploying Longhorn"
   helm upgrade -i longhorn /opt/rancher/helm/longhorn-1.3.2.tgz --namespace longhorn-system --create-namespace --set ingress.enabled=true --set ingress.host=longhorn.$DOMAIN --set global.cattle.systemDefaultRegistry=localhost:5000
 }
 
 # Install and Deploy Neuvector
 function install-neuvector () {
-  echo -e "${BLUE}Rancher Installer ${NC}- Deploying Neuvector"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- Deploying Neuvector"
   helm upgrade -i neuvector --namespace neuvector neuvector/core --create-namespace  --set imagePullSecrets=regsecret --set k3s.enabled=true --set k3s.runtimePath=/run/k3s/containerd/containerd.sock  --set manager.ingress.enabled=true --set controller.pvc.enabled=true --set controller.pvc.capacity=500Mi --set registry=localhost:5000 --set tag=5.0.5 --set controller.image.repository=neuvector/controller --set enforcer.image.repository=neuvector/enforcer --set manager.image.repository=neuvector/manager --set cve.updater.image.repository=neuvector/updater --set manager.ingress.host=neuvector.$DOMAIN
 }
 
 # Validate Complete Deployment
 function validation () {
-  echo -e "${BLUE}Rancher Installer ${NC}- Image List"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- Image List"
   kubectl get pods -A -o jsonpath="{.items[*].spec.containers[*].image}" | tr -s '[[:space:]]' '\n' |sort | uniq -c
 }
 
 # Script Steps and Tips
 function steps () {
-  echo -e "${BLUE}Rancher Installer ${NC}- High Level Steps"
+  echo -e "${BLUE}Rancher Offline Installer ${NC}- High Level Steps"
   echo -e "  1) Download and Build the script on a server with internet access."
   echo -e "  2) Mount/Move/Copy the compressed script to the offline/airgapped server."
   echo -e "  3) Install and Deploy the control node to the first offline/airgapped server."
