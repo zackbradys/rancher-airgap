f8be58d18b3f7a02703b9f7d8f6a713874b0ff70
Zack Hodgson Brady <zackbrady123@gmail.com>
2024-03-17 09:38:44 -0400

    updated harvester scripts

--- hauler/scripts/harvester/hauler-harvester.sh
+++ hauler/scripts/harvester/hauler-harvester.sh
@@ -8,7 +8,11 @@ cd /opt/hauler/harvester
 
 ### Download Harvester Images and Modify the List
 ### https://github.com/harvester/harvester
-harvesterImages=$(curl -sSfL https://github.com/harvester/harvester/releases/download/v${vHarvester}/harvester-images-list.txt | sed -e "/^\s*#/d" -e "/^$/d" -e "s/^/    - name: /" -e "s/docker\.io\///g")
+harvesterImagesAMD64=$(curl -sSfL https://github.com/harvester/harvester/releases/download/v${vHarvester}/harvester-images-list-amd64.txt | sed -e "/^\s*#/d" -e "/^$/d" -e "s/^/    - name: /" -e "s/docker\.io\///g")
+
+### Download Harvester Images and Modify the List
+### https://github.com/harvester/harvester
+harvesterImagesARM64=$(curl -sSfL https://github.com/harvester/harvester/releases/download/v${vHarvester}/harvester-images-list-arm64.txt | sed -e "/^\s*#/d" -e "/^$/d" -e "s/^/    - name: /" -e "s/docker\.io\///g")
 
 ### Create Hauler Manifest
 cat << EOF >> /opt/hauler/harvester/rancher-airgap-harvester.yaml
@@ -22,12 +26,30 @@ spec:
       name: harvester-v${vHarvester}-amd64.iso
     - path: https://releases.rancher.com/harvester/v${vHarvester}/harvester-v${vHarvester}-vmlinuz-amd64
       name: harvester-v${vHarvester}-vmlinuz-amd64
+    - path: https://releases.rancher.com/harvester/v${vHarvester}/harvester-v${vHarvester}-initrd-amd64
+      name: harvester-v${vHarvester}-initrd-amd64
     - path: https://releases.rancher.com/harvester/v${vHarvester}/harvester-v${vHarvester}-rootfs-amd64.squashfs
       name: harvester-v${vHarvester}-rootfs-amd64.squashfs
     - path: https://releases.rancher.com/harvester/v${vHarvester}/harvester-v${vHarvester}-amd64.sha512
       name: harvester-v${vHarvester}-amd64.sha512
     - path: https://releases.rancher.com/harvester/v${vHarvester}/version.yaml
       name: version.yaml
+    - path: https://releases.rancher.com/harvester/v${vHarvester}/harvester-v${vHarvester}-amd64-net-install.iso
+      name: harvester-v${vHarvester}-amd64-net-install.iso
+    - path: https://releases.rancher.com/harvester/v${vHarvester}/harvester-v${vHarvester}-arm64.iso
+      name: harvester-v${vHarvester}-arm64.iso
+    - path: https://releases.rancher.com/harvester/v${vHarvester}/harvester-v${vHarvester}-vmlinuz-arm64
+      name: harvester-v${vHarvester}-vmlinuz-arm64
+    - path: https://releases.rancher.com/harvester/v${vHarvester}/harvester-v${vHarvester}-initrd-arm64
+      name: harvester-v${vHarvester}-initrd-arm64
+    - path: https://releases.rancher.com/harvester/v${vHarvester}/harvester-v${vHarvester}-rootfs-arm64.squashfs
+      name: harvester-v${vHarvester}-rootfs-arm64.squashfs
+    - path: https://releases.rancher.com/harvester/v${vHarvester}/harvester-v${vHarvester}-arm64.sha512
+      name: harvester-v${vHarvester}-arm64.sha512
+    - path: https://releases.rancher.com/harvester/v${vHarvester}/version-arm64.yaml
+      name: version-arm64.yaml
+    - path: https://releases.rancher.com/harvester/v${vHarvester}/harvester-v${vHarvester}-arm64-net-install.iso
+      name: harvester-v${vHarvester}-arm64-net-install.iso
 ---
 apiVersion: content.hauler.cattle.io/v1alpha1
 kind: Images
@@ -35,5 +57,6 @@ metadata:
   name: rancher-airgap-images-harvester
 spec:
   images:
-${harvesterImages}
+${harvesterImagesAMD64}
+${harvesterImagesARM64}
 EOF
