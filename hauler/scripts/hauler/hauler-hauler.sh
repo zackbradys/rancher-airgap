40618147173de4c1bc23bc7c362d973698273dc1
Zack Brady <zackbrady123@gmail.com>
2024-06-16 21:28:02 -0400

    updated hauler artifacts

--- hauler/scripts/hauler/hauler-hauler.sh
+++ hauler/scripts/hauler/hauler-hauler.sh
@@ -19,4 +19,12 @@ spec:
       name: hauler-linux-amd64.tar.gz
     - path: https://github.com/rancherfederal/hauler/releases/download/v${vHauler}/hauler_${vHauler}_linux_arm64.tar.gz
       name: hauler-linux-arm64.tar.gz
+---
+apiVersion: content.hauler.cattle.io/v1alpha1
+kind: Images
+metadata:
+  name: rancher-airgap-images-hauler
+spec:
+  images:
+    - name: ghcr.io/rancherfederal/hauler:v${vHauler}
 EOF
