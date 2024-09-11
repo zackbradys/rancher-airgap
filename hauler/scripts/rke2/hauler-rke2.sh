545f27f4ff28759b22ced5b0dce72723dd916001
Zack Brady <zackbrady123@gmail.com>
2024-09-11 05:40:11 -0500

    updated/fixed rke2

--- hauler/scripts/rke2/hauler-rke2.sh
+++ hauler/scripts/rke2/hauler-rke2.sh
@@ -9,7 +9,14 @@ cd /opt/hauler/rke2
 
 ### Download RKE2 Images and Modify the List
 ### https://github.com/rancher/rke2
-rke2Images=$(curl -sSfL https://github.com/rancher/rke2/releases/download/v${vRKE2}+rke2r1/rke2-images-all.linux-amd64.txt | sed -e "s/docker\.io\///g" -e "s/^/    - name: /")
+rke2ImagesAMD64=$(curl -sSfL https://github.com/rancher/rke2/releases/download/v${vRKE2}+rke2r1/rke2-images-all.linux-amd64.txt | sed -e "s/docker\.io\///g" -e "s/^/    - name: /")
+
+### Download RKE2 Images and Modify the List
+### https://github.com/rancher/rke2
+rke2ImagesARM64=$(curl -sSfL https://github.com/rancher/rke2/releases/download/v${vRKE2}+rke2r1/rke2-images-all.linux-arm64.txt | sed -e "s/docker\.io\///g" -e "s/^/    - name: /")
+
+### Combine RKE2 Image Lists
+rke2Images=$(echo -e "${rke2ImagesAMD64}\n${rke2ImagesARM64}" | sort | uniq)
 
 ### Create Hauler Manifest
 cat << EOF >> /opt/hauler/rke2/rancher-airgap-rke2.yaml
@@ -19,6 +26,8 @@ metadata:
   name: rancher-airgap-files-rke2
 spec:
   files:
+    - path: https://raw.githubusercontent.com/rancher/rke2/release-1.29/install.sh
+      name: install.sh
     - path: https://github.com/rancher/rke2/releases/download/v${vRKE2}%2Brke2r1/rke2-images.linux-amd64.tar.zst
       name: rke2-images.linux-amd64.tar.zst
     - path: https://github.com/rancher/rke2/releases/download/v${vRKE2}%2Brke2r1/rke2.linux-amd64.tar.gz
@@ -27,8 +36,14 @@ spec:
       name: sha256sum-amd64.txt
     - path: https://github.com/rancher/rke2/releases/download/v${vRKE2}%2Brke2r1/rke2.linux-amd64
       name: rke2.linux-amd64
-    - path: https://raw.githubusercontent.com/rancher/rke2/master/install.sh
-      name: install.sh
+    - path: https://github.com/rancher/rke2/releases/download/v${vRKE2}%2Brke2r1/rke2-images.linux-arm64.tar.zst
+      name: rke2-images.linux-arm64.tar.zst
+    - path: https://github.com/rancher/rke2/releases/download/v${vRKE2}%2Brke2r1/rke2.linux-arm64.tar.gz
+      name: rke2.linux-arm64.tar.gz
+    - path: https://github.com/rancher/rke2/releases/download/v${vRKE2}%2Brke2r1/sha256sum-arm64.txt
+      name: sha256sum-arm64.txt
+    - path: https://github.com/rancher/rke2/releases/download/v${vRKE2}%2Brke2r1/rke2.linux-arm64
+      name: rke2.linux-arm64
     - path: https://github.com/rancher/rke2-selinux/releases/download/v${vRKE2SELinux}.stable.1/rke2-selinux-${vRKE2SELinux}-1.el9.noarch.rpm
       name: rke2-selinux-${vRKE2SELinux}-1.el9.noarch.rpm
     - path: https://github.com/rancher/rke2-selinux/releases/download/v${vRKE2SELinux}.stable.1/rke2-selinux-${vRKE2SELinux}-1.el8.noarch.rpm
